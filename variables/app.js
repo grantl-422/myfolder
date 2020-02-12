bcb5cc31c1aa9e0ecf4260955c895d65bff275b9
Grant Labrecque <grantlabrecque@ORRHS.ORG>
2020-02-12 10:10:55 -0500

    new

---
+++ variables/app.js
@@ -0,0 +1,29 @@
+// const add = 7 + 3;
+// const sub = 7 - 3;
+// const mult = 7 * 3;
+// const div = 7 / 3;
+// const paren = (83 + 92 + 86 + 94) / 4;
+
+// let score = 0 
+// let numlikes = 7
+// let lives = 3
+
+// console.log('current Score:'+ score)
+// console.log('So far '+ numlikes + ' People like this page')
+// console.log(lives + ' lives Left. score = '+ score)
+
+// let points = 1023;
+// let lives = 3;
+// let totalTime = 45;
+// console.log("Points: " + points);
+// console.log("Lives: " + lives);
+// console.log("Total Time: " + totalTime + " secs");
+// console.log("---------------");
+// let totalScore = 68.2;
+// console.log("Your Total Score is: " + totalScore);
+
+
+
+
+
+
