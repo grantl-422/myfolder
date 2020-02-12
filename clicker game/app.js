8407b37b1bed10f09ca81b4b2f0b06b5b1280466
Grant Labrecque <grantlabrecque@ORRHS.ORG>
2020-02-12 10:50:39 -0500

    new page

--- clicker game/app.js
+++ clicker game/app.js
@@ -3,14 +3,17 @@ let heading
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    bananaImg = createImg('/banana.png','banana image');
+    bananaImg = createImg('banana.png','banana image');
     bananaImg.size(200,100)
     heading = createElement('h2','click the banana!')
     heading.position(width/3,height/4)
+    frameRate(1)
 }
 function draw(){
 bananaImg.position(random(width),random(height))
-mousePressed(bananaImg)
-
-
-}
+bananaImg.mousePressed(youWon)
+ }
+ function youWon(){
+     heading.html('you won')
+     bananaImg.remove()
+ }
